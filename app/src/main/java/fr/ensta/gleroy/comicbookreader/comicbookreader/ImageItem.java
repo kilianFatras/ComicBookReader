468edaceaefd6233da07950e53bb3efbf59b15d2
Guillaume LEROY <gleroy@ensta.fr>
2017-01-21 14:58:12 +0100

    Begin to work on the interface

---
+++ app/src/main/java/fr/ensta/gleroy/comicbookreader/comicbookreader/ImageItem.java
@@ -0,0 +1,34 @@
+package fr.ensta.gleroy.comicbookreader.comicbookreader;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by gleroy on 20/01/17.
+ */
+
+public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
